6ba6bc598a8eb26e2fa3890d1b2f2842f6a7fd09
Hauke Henke <haukeh@ad.itm.kth.se>
2018-10-15 14:37:21 +0200

    Initial version

---
+++ squares.py
@@ -0,0 +1,25 @@
+
+# coding: utf-8
+
+
+
+
+# In[1]:
+#li = [1, 2, 3, 4]
+
+def list_squares(n):
+    "returns a lists of squares"
+    li = []
+    for i in range(n):
+        #print(i**2, end='')
+        li.append(i**2)
+    #print()
+    return li
+
+for i in list_squares(15):
+    print(i, end=' ')
+
+
+
+
+
