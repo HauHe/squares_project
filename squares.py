03103ac54b9674b9e12ab9d0f0397a7b74f7d9b9
Hauke Henke <haukeh@ad.itm.kth.se>
2018-10-15 15:05:18 +0200

    Introducefunction

--- squares.py
+++ squares.py
@@ -1,23 +1,13 @@
 
-# coding: utf-8
-
-hello = "Hello world"
-
-
-# In[1]:
-#li = [1, 2, 3, 4]
-
 def list_squares(n):
     "returns a lists of squares"
     li = []
-    for i in range(n):
-        #print(i**2, end='')
+    for i in range(1, n+1):
         li.append(i**2)
-    #print()
     return li
 
-for i in list_squares(15):
-    print(i, end=' ')
+for i in list_squares(4):
+    print(i)
 
 
 
