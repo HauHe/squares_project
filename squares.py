c3243cd2c6d4f1983cad12d1de6612b1dcd40895
Hauke Henke <haukeh@ad.itm.kth.se>
2018-10-15 16:02:07 +0200

    Reduced code to functions without squares Please enter the commit message for your changes. Lines starting

--- squares.py
+++ squares.py
@@ -1,14 +1,3 @@
-<<<<<<< Updated upstream
-
-def list_squares(n):
-    "returns a lists of squares"
-    li = []
-    for i in range(1, n+1):
-        li.append(i**2)
-    return li
-
-for i in list_squares(4):
-=======
 def list_powers(n, m):
     "returns a lists of squares"
     li = []
