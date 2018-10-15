119e92126d8d7318cbe743d924522213372d314c
Hauke Henke <haukeh@ad.itm.kth.se>
2018-10-15 15:42:52 +0200

    Changed to general function instead of squaring

--- squares.py
+++ squares.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 
 def list_squares(n):
     "returns a lists of squares"
@@ -7,6 +8,16 @@ def list_squares(n):
     return li
 
 for i in list_squares(4):
+=======
+def list_powers(n, m):
+    "returns a lists of squares"
+    li = []
+    for i in range(1, n+1):
+        li.append(i**m)
+    return li
+
+for i in list_powers(4, 3):
+>>>>>>> Stashed changes
     print(i)
 
 
